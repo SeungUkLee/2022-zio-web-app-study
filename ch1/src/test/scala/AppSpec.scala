0d98c735e2de4604b355d9402a8e39cd64582e2b
SeungukLee <seunguk@seunguk.dev>
2022-09-02 05:53:45 +0900

    add day 01

--- ch1/src/test/scala/AppSpec.scala
+++ ch1/src/test/scala/AppSpec.scala
@@ -1,16 +1,22 @@
 import zio.test._
 
+import java.io.IOException
+
 // console를 테스트해보세요
 // https://zio.dev/reference/test/
 // https://zio.dev/reference/test/services/console/
 
 object AppSpec extends ZIOSpecDefault {
-  override def spec = suite("App")(
-    test("console test") {
-      assertTrue(true)
-    },
-    test("your test") {
-      ???
-    }
-  )
+  override def spec: Spec[Any, IOException] =
+    suite("App")(
+      test("Console Test") {
+        for {
+          _ <- TestConsole.feedLines("Seunguk")
+          _ <- App.prog
+          lines <- TestConsole.output
+        } yield assertTrue(
+          lines == Vector("Please enter your name: ", "Hello, Seunguk!\n")
+        )
+      }
+    )
 }
